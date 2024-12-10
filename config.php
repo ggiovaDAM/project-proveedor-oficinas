e8baf70bc6335e84511a6096afbca17d486d9028
Guido Giova <guido.giovaDAM@gmail.com>
2024-12-10 09:14:48 +0100

    feat: add error page with URL constant
    Created a simple error page to show error messages, utilizing sessions to pass the text.
    Created the URL constant in config.php to make accessing the web pages globally constant.

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+    define("ROOT_URL", "http://localhost");
+
+        define("SOURCE_DIR_URL", ROOT_URL . "/src");
+
+            define("PAGES_DIR_URL", SOURCE_DIR_URL . "/pages");
+
+                define("ERROR_DIR_URL", PAGES_DIR_URL . "/error");
+
+                    define("ERROR_URL", ERROR_DIR_URL . "/error.php");
